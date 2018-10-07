388f2b574404b9b91fa6eacc4d2af94c10014980
Alex Popescu <alexopescuneo@gmail.com>
2018-10-07 13:01:59 +0300

    Changed power setters to use mathematics

--- TeamCode/src/main/java/TeleOpTest.java
+++ TeamCode/src/main/java/TeleOpTest.java
@@ -1,13 +1,11 @@
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Test")
 
 public class TeleOpTest extends OpMode {
-
-    private static final float   ON = 1;
-    //private static final float   OFF = 0;
 
     private DcMotor Motor_left = null;
     private DcMotor Motor_right = null;
@@ -21,43 +19,20 @@ public class TeleOpTest extends OpMode {
         Motor_right.setDirection(DcMotor.Direction.REVERSE);
     }
 
-    public void move(String MovementType, String Movement)  {
-        if (MovementType == "linear")   {
-            if (Movement == "forward")
-            {
-                Motor_left.setPower(ON);
-                Motor_right.setPower(ON);
-            }
-            else if (Movement == "backward") {
-                Motor_left.setPower(-ON);
-                Motor_right.setPower(-ON);
-            }
-        }
-        else if (MovementType == "rotate") {
-            if (Movement == "right") {
-                Motor_right.setPower(ON);
-                Motor_left.setPower(-ON);
-            } else if (Movement == "left") {
-                Motor_right.setPower(-ON);
-                Motor_left.setPower(ON);
-            }
-        }
-    }
-
     @Override
     public void loop()  {
-        if (gamepad1.left_stick_y == -1)    {
-            move("linear", "forward");
-        }
-        if (gamepad1.left_stick_y == 1 )    {
-            move("linear", "backward");
-        }
-        if (gamepad1.right_stick_x == 1)    {
-            move("rotate", "right");
-        }
-        if (gamepad1.right_stick_x == -1)   {
-            move("rotate", "left");
-        }
+
+        double rightPower;
+        double leftPower;
+
+        double drive = -gamepad1.left_stick_y;
+        double turn  =  gamepad1.right_stick_x;
+
+        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
+        Motor_left.setPower(leftPower);
+        Motor_right.setPower(rightPower);
     }
 
     @Override
