38c930d00ddbe0770ca5465f7b25f5914029f99a
Alex Popescu <alexpopescuneo@gmail.com>
2018-10-20 18:16:51 +0300

    added shovel controls

--- TeamCode/src/main/java/TeleOpTest_single_stick.java
+++ TeamCode/src/main/java/TeleOpTest_single_stick.java
@@ -2,6 +2,7 @@
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Test_single_stick")
@@ -9,6 +10,9 @@ public class TeleOpTest_single_stick extends OpMode
 {
     private DcMotor Motor_left = null;
     private DcMotor Motor_right = null;
+
+    private DcMotor arm1 = null;
+    private DcMotor arm2 = null;
 
     public void setPowerChassis(double leftPower, double rightPower)    {
         Motor_left.setPower(leftPower);
@@ -20,8 +24,14 @@ public class TeleOpTest_single_stick extends OpMode
         Motor_left  = hardwareMap.get(DcMotor.class, "left_drive");
         Motor_right = hardwareMap.get(DcMotor.class, "right_drive");
 
+        arm1 = hardwareMap.get(DcMotor.class, "arm1");
+        arm2 = hardwareMap.get(DcMotor.class, "arm2");
+
         Motor_left.setDirection(DcMotor.Direction.FORWARD);
         Motor_right.setDirection(DcMotor.Direction.REVERSE);
+
+        arm1.setDirection(DcMotor.Direction.FORWARD);
+        arm2.setDirection(DcMotor.Direction.FORWARD);
     }
 
     @Override
@@ -30,10 +40,14 @@ public class TeleOpTest_single_stick extends OpMode
         double drive = -gamepad1.left_stick_y;
         double turn  =  gamepad1.left_stick_x;
 
+        double move1 = -gamepad1.right_stick_y;
+
         double leftPower = Range.clip(drive + turn, -1.00, 1.00);
         double rightPower = Range.clip(drive - turn, -1.00, 1.00);
         setPowerChassis(leftPower, rightPower);
 
+        arm1.setPower(move1);
+        arm2.setPower(-move1);
     }
 
     @Override
